79bf3c056085c48df45420512064e8ae336c9387
Aryan <118302608+Aryan3522@users.noreply.github.com>
2026-03-02 13:57:47 +0530

    feat: improve cross origin access

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const todoRouter = require("./Routes/todo");
 const authRouter = require("./Routes/auth");
 const { connectDB } = require("./Config/db");
-require('dotenv').config()
+require("dotenv").config();
 
 connectDB();
 
@@ -12,17 +12,21 @@ const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.json({ 
-  origin: 'https://todo-list-mu-mauve-77.vercel.app/',  
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
 
 app.use("/todo", todoRouter);
 app.use("/auth", authRouter);
 
-app.get("/", (req,res)=>{
-  res.send({mgs:"Server is ready to use"})
+app.get("/", (req, res) => {
+  res.send({ mgs: "Server is ready to use" });
 });
 
 app.listen(process.env.PORT, () => {
